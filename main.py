d56a5b7d0bf6faf61fb0299e4685253f41b7773c
RMensinck <remcomensinck@hotmail.com>
2021-11-19 17:51:08 +0100

    mutability bug fixed

--- main.py
+++ main.py
@@ -1,29 +1,42 @@
+from copy import Error, copy
 import random
+import time
+import matplotlib.pyplot as plt
 
-GENERATION_SIZE = 10
+GENERATION_SIZE = 30  #must be even number
 NUMBER_OF_GENERATIONS = 5
-
-random.seed(1)
 
 
 class Genome:
-    def __init__(self, x, y, z):
-        self.x = x
-        self.y = y
-        self.z = z
+    def __init__(self, identifier):
+        self.id = identifier
+        self.times_mutated = 0
+        self.x = random.randint(-100, 100)
+        self.y = random.randint(-100, 100)
+        self.z = random.randint(-100, 100)
         self.cost = self.cost_function()
 
-    def mutations(self):
-        if random.randint(1, 100) < 5:
+    def mutate(self):
+        if random.randint(1, 100) < 2:
             random_int = random.randint(1, 3)
             if random_int == 1: self.x = random.randint(-100, 100)
             if random_int == 2: self.y = random.randint(-100, 100)
             if random_int == 3: self.z = random.randint(-100, 100)
             self.cost = self.cost_function()
-            print("mutation occured---------------")
+            return True
+        return False
 
     def cost_function(self):
         return abs(0 - problem(self.x, self.y, self.z))
+
+    def __str__(self):
+        return f"{self.id}.{self.times_mutated}: ({self.x}, {self.y}, {self.z}) costs {self.cost}"
+
+
+def check_validity_constants():
+    if GENERATION_SIZE % 2 != 0 or GENERATION_SIZE < 0:
+        print("Generation size must be a even, positive int")
+        return False
 
 
 def problem(x, y, z):
@@ -31,27 +44,26 @@ def problem(x, y, z):
 
 
 def create_init_genomes():
-    genomes = []
-    for _ in range(GENERATION_SIZE):
-        genomes.append(
-            Genome(random.randint(-100, 100), random.randint(-100, 100),
-                   random.randint(-100, 100)))
-    return genomes
+    return [Genome(id) for id in range(GENERATION_SIZE)]
 
 
 def tournament_selection(input_generation):
-    next_gen = []
-    for _ in range(GENERATION_SIZE):
-        participants = [
-            input_generation[random.randint(0, GENERATION_SIZE - 1)],
-            input_generation[random.randint(0, GENERATION_SIZE - 1)]
-        ]
-        if participants[0].cost < participants[1].cost:
-            winner = participants[0]
-        else:
-            winner = participants[1]
-        next_gen.append(winner)
-    return next_gen
+    participant1 = input_generation[random.randint(0, GENERATION_SIZE - 1)]
+    participant2 = input_generation[random.randint(0, GENERATION_SIZE - 1)]
+    participant3 = input_generation[random.randint(0, GENERATION_SIZE - 1)]
+    participant4 = input_generation[random.randint(0, GENERATION_SIZE - 1)]
+
+    if participant1.cost < participant2.cost:
+        winner1 = participant1
+    else:
+        winner1 = participant2
+
+    if participant3.cost < participant4.cost:
+        winner2 = participant3
+    else:
+        winner2 = participant4
+
+    return winner1, winner2
 
 
 def generation_avarage_cost(generation):
@@ -61,19 +73,59 @@ def generation_avarage_cost(generation):
     return sum / len(generation)
 
 
+def crossover(genome1, genome2):
+
+    if random.randint(1, 100) <= 20:
+        if random.choice([True, False]):
+            x1 = genome1.x
+            x2 = genome2.x
+            genome1.x = x2
+            genome2.x = x1
+        if random.choice([True, False]):
+            y1 = genome1.y
+            y2 = genome2.y
+            genome1.y = y2
+            genome2.y = y1
+        if random.choice([True, False]):
+            z1 = genome1.z
+            z2 = genome2.z
+            genome1.z = z2
+            genome2.z = z1
+
+
 def main():
+
+    if check_validity_constants() == False: raise Error
 
     genomes = create_init_genomes()
 
-    for _ in range(NUMBER_OF_GENERATIONS):
-        print("\n")
+    for generation in range(NUMBER_OF_GENERATIONS):
         for genome in genomes:
-            print(f"pre-mutatie: {genome.x} {genome.y} {genome.z}")
-            genome.mutations()
-            print(f"post-mutatie: {genome.x} {genome.y} {genome.z}")
-        genomes = tournament_selection(genomes)
+            genome.mutate()
 
-        print(generation_avarage_cost(genomes))
+        next_gen = []
+        for _ in range(GENERATION_SIZE // 2):
+            winners = tournament_selection(genomes)
+            crossover(winners[0], winners[1])
+            next_gen.append(copy(winners[0]))
+            next_gen.append(copy(winners[1]))
+        genomes = next_gen
+
+        costs = []
+        pos = []
+        genomes.sort(key=lambda x: x.cost)
+        for i, genome in enumerate(genomes):
+            costs.append(genome.cost)
+            pos.append(i)
+
+            plt.ylim(0, 500000)
+            plt.scatter(pos, costs, color="black")
+            plt.pause(0.001)
+
+        print(
+            f"Generation {generation} done, average cost: {generation_avarage_cost(genomes)}"
+        )
+    plt.show()
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -1,32 +1,24 @@
-import random
-from main import cost, create_init_genomes
-"""
-genomes = create_init_genomes()
-genomes_with_cost = []
-
-for genome in genomes:
-    genomes_with_cost.append([cost(genome[0], genome[1], genome[2]), genome])
-
-print(genomes_with_cost)
-"""
-genomes_with_cost = [[94922142, [60, 83, -90]], [94922142, [60, 83, -90]],
-                     [94922142, [60, 83, -90]], [94922142, [60, 83, -90]],
-                     [94922142, [60, 83, -90]], [94922142, [60, 83, -90]],
-                     [94922142, [60, 83, -90]], [94922142, [60, 83, -90]],
-                     [94922142, [60, 83, -90]], [94922142, [60, 83, -90]]]
-print("BEFORE")
-print(genomes_with_cost)
+class Aclass:
+    def __init__(self):
+        self.x = 29
 
 
-def mutations(generation):
-    for genome in generation:
-        random_int = random.randint(1, 15)
-        if random_int < 2:
-            print("MUTATION OCCURED")
-            genome[1][random.randint(0, 2)] = random.randint(-100, 100)
-            genome[0] = cost(genome[1][0], genome[1][1], genome[1][2])
-    return generation
+class Bclass:
+    def __init__(self):
+        self.y = 344
 
 
-print("AFTER")
-print(mutations(genomes_with_cost))
+a = Aclass()
+b = Bclass()
+
+print(a)
+print(b)
+
+list = [a, b]
+
+newlist = []
+
+for x in range(5):
+    newlist.append(a)
+
+print(newlist)
